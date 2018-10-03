40bf97f128060a202c7748d41db99ce7f0e73e32
fanjiahe <fanjiahe@ziggurat.cn>
2018-10-03 11:30:02 +0000

    use agent to test API

--- test/app-test.js
+++ test/app-test.js
@@ -1,18 +1,18 @@
+'use strict';
+
 require('should');
 const app = require('../src/index');
-const server = app.listen(app.port);
+const server = app.listen(8080);
 const request = require('supertest').agent(server);
 
 describe('Application Tests', function() {
   after(function() {
     server.close();
   });
-
   it('GET request', function(done) {
     request
     .get('/')
     .expect(200)
-    .expect('content-type', 'application/json; charset=utf-8')
     .expect('[]', done);
   });
 });
