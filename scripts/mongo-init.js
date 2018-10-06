58f94493f3062ab13e179ebe80c6365bcca5a921
fanjiahe <fanjiahe@ziggurat.cn>
2018-10-06 08:00:57 +0000

    add plugin loader

---
+++ scripts/mongo-init.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node;
+
+/**
+ * initialize default admin user
+ *  - use mongoose to insert the user;
+ *  - use the mongo client to add the user
+ * */
---
+++ src/libraries/plugin-loader.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const fs =  require("fs");
+const path = require("path");
+const compose = require("koa-compose");
+
+function getDirs (pluginPath) {
+  return fs.readdirSync(pluginPath).filter(file => {
+    return fs.statSync(path.join(pluginPath, file)).isDirectory()
+  })
+}
+
+module.exports = (pluginPath, filename = 'index.js') => {
+  let plugins = {}
+  let dirs = getDirs(pluginPath)
+  let list = []
+
+  for (let name of dirs) {
+    let fn = require(path.join(pluginPath, name, filename))
+
+    if (typeof fn !== 'function' && typeof fn.default === 'function') {
+      fn = fn.default
+    } else {
+      throw (new Error('plugin must be a function!'))
+    }
+
+    plugins[name] = fn
+
+    list.push(function (ctx, next) {
+      return fn(ctx, next) || next()
+    })
+  }
+
+  return compose(list)
+}
