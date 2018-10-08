1b405843d1a62e3dfb705168886f73ea9174cf6f
fanjiahe <fanjiahe@ziggurat.cn>
2018-10-08 06:12:28 +0000

    default user generation

---
+++ scripts/generate-password.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+const stringUtil = require("../src/libraries/string");
+
+const password = "pasw0rd";
+
+var hashedPassword = stringUtil.generatePasswordHash(password);
+
+console.log(hashedPassword);
