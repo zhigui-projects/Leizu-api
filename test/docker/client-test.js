dd94ee1f56bf489e60f324ed01f9cb4a2d87758f
fanjiahe <fanjiahe@ziggurat.cn>
2018-10-16 08:00:20 +0000

    update the docker test codes

--- test/docker/client-test.js
+++ test/docker/client-test.js
@@ -4,13 +4,28 @@ const Docker = require('dockerode');
 const connectOptions = {
     protocol: 'http',
     host: "59.110.164.211",
-    port: 7060
+    port: 7061
 };
 
 var docker = new Docker(connectOptions);
 
+/*
 docker.info().then(function(info){
     console.log(info);
 },function(err){
     console.error(err);
 });
+*/
+
+async function getDockerVersion(docker){
+    try{
+        let version = await docker.version();
+        console.log(version);
+    }catch(err){
+        console.error("<<<< capture the errors >>>>")
+        console.error(err);
+    }
+    
+}
+
+getDockerVersion(docker);
