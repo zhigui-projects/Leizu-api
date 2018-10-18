520517c6776c197277c9f7b78d10e221b0f7f98f
fanjiahe <fanjiahe@ziggurat.cn>
2018-10-18 01:18:37 +0000

    add the channel creation and update feature

--- src/app/fabric/channel.js
+++ src/app/fabric/channel.js
@@ -2,8 +2,9 @@
 
 const Channel = require('../../models/channel');
 const common = require('../../libraries/common');
-
+const ChannelService = require("../../services/fabric/channel");
 const router = require('koa-router')({prefix: '/channel'});
+
 router.get('/', async ctx => {
     await Channel.find({}, (err, docs) => {
         if (err) {
@@ -33,20 +34,39 @@ router.get('/:id', async ctx => {
 
 /**
  * fn: add the new channel into fabric network 
- *
+ * parameters: genesis block
+ * 
  */ 
-
-router.post("/new",async ctx =>{
-    ctx.body = "todo";
+router.post("/",async ctx =>{
+    let parameters = ctx.request.body;
+    try{
+        let channelService = ChannelService.getInstance();
+        await channelService.createChannel(parameters);
+        let dto = {};
+        let channel = channelService.addChannelIntoDb(dto);
+        ctx.body = common.success(channel, common.SUCCESS);
+    }catch(err){
+        ctx.status = 400;
+        ctx.body = common.error({}, err.message);
+    }
 });
 
 /**
  * fn: update existing channel's configuration
+ * parameters: configuration
  * 
  */ 
-
 router.put("/:id",async ctx =>{
-    ctx.body = "todo"
+    let id = ctx.params.id;
+    let params = ctx.request.body;
+    try{
+        let channelService = new ChannelService();
+        await channelService.updateChannel(id,params);
+        ctx.body = common.success({id:id}, common.SUCCESS);
+    }catch(err){
+       ctx.status = 400;
+       ctx.body = common.error({}, err.message); 
+    }
 });
 
 
---
+++ src/services/fabric/channel.js
@@ -0,0 +1,25 @@
+"use strict";
+
+module.exports = class ChannelService {
+    
+    constructor(){
+        
+    }
+    
+    static getInstance(){
+        let channelService = new ChannelService();
+        return channelService;
+    }
+    
+    async createChannel(params){
+        
+    }
+    
+    async updateChannel(id,params){
+        
+    }
+    
+    async addChannelIntoDb(dto){
+        
+    }
+}
