e846f4fa38398196ff61aaff6f4124129c4a1045
Max Peng <pengisgood@gmail.com>
2018-10-25 14:44:10 +0800

    rename 'name' to 'username' to align with model field

--- src/app/user/login.js
+++ src/app/user/login.js
@@ -8,35 +8,34 @@ const string = require('../../libraries/string');
 const router = require('koa-router')({prefix: '/user'});
 
 router.post('/login', async (ctx) => {
-    ctx.checkBody('name').notEmpty('Name field is required').len(4, 50, 'Name length must be between 4 and 50 characters');
+    ctx.checkBody('username').notEmpty('Name field is required').len(4, 50, 'Name length must be between 4 and 50 characters');
     ctx.checkBody('password').notEmpty('Password field is required').len(4, 20, 'Password length must be between 4 and 20 characters');
 
     if (ctx.errors) throw new BadRequest(ctx.errors);
 
-    const {name, password} = ctx.request.body;
-    const user = await getUser(name, password);
+    const {username, password} = ctx.request.body;
+    const user = await getUser(username, password);
 
     ctx.body = {
         id: user._id,
-        name: user.toJSON().name,
+        username: user.toJSON().username,
         token: jwt.encode({id: user._id}),
     };
 });
 
 router.post('/password/reset', async (ctx) => {
-    const {name, password, newPassword} = ctx.request.body;
-    const user = await getUser(name, password);
-    const newUser = await User.findOneAndUpdate(user._id, {password: string.generatePasswordHash(newPassword)}, {new: true});
-
+    const {username, password, newPassword} = ctx.request.body;
+    const user = await getUser(username, password);
+    const newUser = await User.findOneAndUpdate({_id: user._id}, {password: string.generatePasswordHash(newPassword)}, {new: true});
     ctx.body = {
         id: newUser._id,
-        name: newUser.toJSON().name
+        username: newUser.toJSON().username
     };
 });
 
 const getUser = async (username, password) => {
     const user = await User.findOne({
-        name: username,
+        username: username,
         password: string.generatePasswordHash(password),
     });
 
