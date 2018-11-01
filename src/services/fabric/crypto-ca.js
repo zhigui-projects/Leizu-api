570611294648ea0a3d05f835ef0b0048d663b39d
fanjiahe <fanjiahe@ziggurat.cn>
2018-11-01 14:07:00 +0000

    refactor codes

--- src/services/fabric/crypto-ca.js
+++ src/services/fabric/crypto-ca.js
@@ -119,7 +119,7 @@ module.exports = class CryptoCA {
     	}
     }
     
-    async postContainerStart(params){
+    async postContainerStart(){
         let result = {};
         if(!this.bootstrapEnrollment){
             await this.bootstrapUserEnrollement();
