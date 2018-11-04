0a3055ec0ad4aad2512ac19d6210eb13b802abdd
fanjiahe <fanjiahe@ziggurat.cn>
2018-11-05 07:53:03 +0800

    add new columns into the document schema

--- src/models/organization.js
+++ src/models/organization.js
@@ -4,8 +4,9 @@ const mongoose = require('mongoose');
 const organizationSchema = new mongoose.Schema({
     uuid: String,
     name: String,
-    tls: Boolean,
     msp_id: String,
+    admin_key: String,
+    admin_cert: String,
     consortium_id: mongoose.Schema.ObjectId,
     date: {
         type: Date,
