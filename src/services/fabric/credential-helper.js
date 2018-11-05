55ab9ca3a61ad0dbdd1467f99b8d1d9707b425ce
fanjiahe <fanjiahe@ziggurat.cn>
2018-11-05 16:15:40 +0800

    implements the abstract definition

--- src/services/fabric/credential-helper.js
+++ src/services/fabric/credential-helper.js
@@ -2,6 +2,7 @@
 
 const fs = require('fs');
 const path = require('path');
+const stringUtil = require('../../libraries/string-util');
 
 module.exports.CERT_PATHS = {
     cacerts: "cacerts",
@@ -19,44 +20,85 @@ module.exports.CredentialHelper = class {
     }
 
     writeCaCerts(caCert){
+        let dirName = path.join(this.dirName,exports.CERT_PATHS.cacerts);
+        if(this.isDirExists(dirName)){
+            this.removeDir(dirName);
+        }
+        this.createDir(dirName);
+        let filePath = path.join(dirName,"ca-cert.pem");
+        this.writeFile(filePath,caCert);
+    }
 
-
+    writeTlsCaCerts(){
+        let dirName = path.join(this.dirName,exports.CERT_PATHS.tlscacerts);
+        if(this.isDirExists(dirName)){
+            this.removeDir(dirName);
+        }
+        this.createDir(dirName);
+        let filePath = path.join(dirName,"cert.pem");
+        this.writeFile(filePath,caCert);
     }
 
     writeAdminCerts(adminCert){
-
+        let dirName = path.join(this.dirName,exports.CERT_PATHS.admincerts);
+        if(this.isDirExists(dirName)){
+            this.removeDir(dirName);
+        }
+        this.createDir(dirName);
+        let filePath = path.join(dirName,"admin-cert.pem");
+        this.writeFile(filePath,adminCert);
     }
 
     writeSignCerts(signCert){
-
+        let dirName = path.join(this.dirName,exports.CERT_PATHS.signcerts);
+        if(this.isDirExists(dirName)){
+            this.removeDir(dirName);
+        }
+        this.createDir(dirName);
+        let filePath = path.join(dirName,"sign-cert.pem");
+        this.writeFile(filePath,signCert);
     }
 
-    writeTlsCert(tlsCerts){
-
+    writeTlsCert(tls){
+        let dirName = path.join(this.dirName,'tls');
+        if(this.isDirExists(dirName)){
+            this.removeDir(dirName);
+        }
+        this.createDir(dirName);
+        let filePath = path.join(dirName,"cert.pem");
+        this.writeFile(filePath,tls.cert);
+        let keyPath = path.join(dirName,"key.pem");
+        this.writeFile(keyPath,tls.key);
     }
 
     writeKeys(key){
-
+        let dirName = path.join(this.dirName,exports.CERT_PATHS.keystore);
+        if(this.isDirExists(dirName)){
+            this.removeDir(dirName);
+        }
+        this.createDir(dirName);
+        let filePath = path.join(dirName,stringUtil.hash(key)+ ".pem");
+        this.writeFile(filePath,key);
     }
-    
-    isDirExists(dirName){
 
+    isDirExists(dirName){
+        return fs.existsSync(dirName);
     }
 
     removeDir(dirName){
-
+        fs.rmdirSync(dirName);
     }
 
     createDir(dirName){
-
+        fs.mkdirSync(dirName);
     }
 
     writeFile(filePath, fileContent){
-
+        fs.writeFileSync(filePath,fileContent);
     }
 
     removeFile(filePath){
-
+        fs.unlinkSync(filePath);
     }
 
     zipDirectoryFiles(){
