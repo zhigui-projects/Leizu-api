58d2e8639bf1d4d8afe2c38f527209aae5fe7e7f
zhangtailin <zhangtailin@ziggurat.cn>
2018-11-06 22:41:25 +0800

    put create new consortium config to db

--- src/app/fabric/consortium.js
+++ src/app/fabric/consortium.js
@@ -6,6 +6,7 @@ const common = require('../../libraries/common');
 const DbService = require('../../services/db/dao');
 const query = require('../../services/fabric/query');
 const router = require('koa-router')({prefix: '/consortium'});
+const ConsortiumConfig = require('../../services/db/consortium-config');
 
 router.get('/', async ctx => {
     try {
@@ -65,6 +66,17 @@ router.post('/', async ctx => {
     };
     try {
         let consortium = await DbService.addConsortium(dto);
+        ctx.body = common.success(consortium, common.SUCCESS);
+    } catch (err) {
+        ctx.status = 400;
+        ctx.body = common.error({}, err.message);
+    }
+});
+
+router.post('/create', async ctx => {
+    try {
+        var consortiumConfig = new ConsortiumConfig();
+        let consortium = await consortiumConfig.load(ctx.request.body);
         ctx.body = common.success(consortium, common.SUCCESS);
     } catch (err) {
         ctx.status = 400;
---
+++ src/services/db/consortium-config.js
@@ -0,0 +1,101 @@
+'use strict';
+
+const uuid = require('uuid/v1');
+const ConsortiumCreate = require('../../models/create-consortium');
+
+var ConsortiumConfig = class {
+
+    constructor() {
+        this._consortium_config = null;
+    }
+
+    async load(dto) {
+        let consortium = new ConsortiumCreate();
+        consortium.uuid = uuid();
+        consortium.serviceName = dto.serviceName;
+        consortium.version = dto.version;
+        consortium.serviceType = dto.serviceType;
+        consortium.ledgerDB = dto.ledgerDB;
+        consortium.consensusType = dto.consensusType;
+        consortium.kafka = dto.kafka;
+        consortium.zookeeper = dto.zookeeper;
+        consortium.organizations = dto.organizations;
+        consortium.channel = JSON.stringify(dto.channel);
+        consortium = await consortium.save();
+        this._consortium_config = consortium;
+        return consortium;
+    }
+
+    getServiceType() {
+        return this._consortium_config.serviceType;
+    }
+
+    getLedgerDB() {
+        return this._consortium_config.ledgerDB;
+    }
+
+    getConsensusType() {
+        return this._consortium_config.consensusType;
+    }
+
+    getKafka() {
+        return this._consortium_config.kafka;
+    }
+
+    getZookeeper() {
+        return this._consortium_config.zookeeper;
+    }
+
+    getOrganizations() {
+        return this._consortium_config.organizations;
+    }
+
+    getChannel() {
+        return JSON.parse(this._consortium_config.channel);
+    }
+
+    static findServiceType(consortiumId) {
+        var where = {_id: consortiumId};
+        var query = {serviceType: 1};
+        return ConsortiumCreate.find(where, query);
+    };
+
+    static findLedgerDB(consortiumId) {
+        var where = {_id: consortiumId};
+        var query = {ledgerDB: 1};
+        return ConsortiumCreate.find(where, query);
+    };
+
+    static findConsensusType(consortiumId) {
+        var where = {_id: consortiumId};
+        var query = {consensusType: 1};
+        return ConsortiumCreate.find(where, query);
+    };
+
+    static findKafka(consortiumId) {
+        var where = {_id: consortiumId};
+        var query = {kafka: 1};
+        return ConsortiumCreate.find(where, query);
+    };
+
+    static findZookeeper(consortiumId) {
+        var where = {_id: consortiumId};
+        var query = {zookeeper: 1};
+        return ConsortiumCreate.find(where, query);
+    };
+
+    static findOrganizations(consortiumId) {
+        var where = {_id: consortiumId};
+        var query = {organizations: 1};
+        return ConsortiumCreate.find(where, query);
+    };
+
+    static findChannel(consortiumId) {
+        var where = {_id: consortiumId};
+        var query = {channel: 1};
+        return ConsortiumCreate.find(where, query);
+    };
+
+};
+
+module.exports = ConsortiumConfig;
