30595eeaa2f3a338c264e985e9b366ffc1835b7e
Max Peng <pengisgood@gmail.com>
2018-11-08 15:59:43 +0800

    refactor: extract method

--- src/app/fabric/peer.js
+++ src/app/fabric/peer.js
@@ -104,34 +104,9 @@ router.post('/', async ctx => {
             parameters = utils.generatePeerContainerCreateOptions(containerOptions);
         }
 
-        const certs = {
-            adminKey: org.admin_key,
-            adminCert: org.admin_cert,
-            rootCert: org.root_cert
-        };
-        const certPath = await CredentialHelper.storeCredentials(org.msp_id, certs);
-        const client = DockerClient.getInstance(connectionOptions);
-        const remotePath = `${common.PEER_HOME}/${org.msp_id}.zip`;
-        await client.transferFile({
-            local: certPath,
-            remote: remotePath
-        });
-        await DockerClient.getInstance(Object.assign(connectionOptions, {cmd: 'unzip'})).exec([
-            '-o', remotePath, '-d', `${common.PEER_HOME}/data/msp`
-        ]);
-        await DockerClient.getInstance(Object.assign(connectionOptions, {cmd: 'mkdir'})).exec([
-            '-p', `${common.PEER_HOME}/data/tls`
-        ]);
-        await DockerClient.getInstance(Object.assign(connectionOptions, {cmd: 'bash'})).exec([
-            '-c', `cp ${common.PEER_HOME}/data/msp/signcerts/* ${common.PEER_HOME}/data/tls/server.crt`
-        ]);
-        await DockerClient.getInstance(Object.assign(connectionOptions, {cmd: 'bash'})).exec([
-            '-c', `cp ${common.PEER_HOME}/data/msp/keystore/* ${common.PEER_HOME}/data/tls/server.key`
-        ]);
-        await DockerClient.getInstance(Object.assign(connectionOptions, {cmd: 'bash'})).exec([
-            '-c', `cp ${common.PEER_HOME}/data/msp/cacerts/* ${common.PEER_HOME}/data/tls/ca.pem`
-        ]);
+        await PeerService.preContainerStart({org, connectionOptions});
 
+        const client = DockerClient.getInstance(connectionOptions);
         const container = await client.createContainer(parameters);
         if (container) {
             const peer = await DbService.addPeer({
@@ -160,8 +135,7 @@ router.put('/:id', async ctx => {
     let id = ctx.params.id;
     let params = ctx.request.body;
     try {
-        let peerService = new PeerService();
-        await peerService.joinChannel(id, params);
+        await PeerService.joinChannel(id, params);
         ctx.body = common.success({id: id}, common.SUCCESS);
     } catch (err) {
         ctx.status = 400;
