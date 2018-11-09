f164f35d7873f83f18a485cc12a10ec4e31cc16d
fanjiahe <fanjiahe@ziggurat.cn>
2018-11-09 09:27:39 +0800

    add request handler to handler consortium creation request

---
+++ src/services/handler/request.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const Handler = require('./handler');
+
+module.exports = class RequestHandler extends Handler {
+
+    constructor(ctx){
+        super(ctx);
+    }
+
+    async preRequest(ctx){
+
+    }
+
+    async handlerRequest(ctx){
+
+    }
+
+    async postRequest(ctx){
+
+    }
+
+};
+
