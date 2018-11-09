3c4ec8df583675a68cc3cc4eea22b21c65480aef
fanjiahe <fanjiahe@ziggurat.cn>
2018-11-09 08:51:11 +0800

    add http handler to remove code blocks in the router

---
+++ src/services/handler/handler.js
@@ -0,0 +1,21 @@
+'use strict';
+
+
+module.exports = class HttpHandler{
+
+    constructor(){
+
+    }
+
+    async pre(ctx){
+
+    }
+
+    async handle(ctx){
+
+    }
+
+    async post(ctx){
+
+    }
+};
