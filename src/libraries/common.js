e00f63305f75831d50c9ad7023caea5fb3543745
fanjiahe <fanjiahe@ziggurat.cn>
2018-11-12 09:48:39 +0800

    add the request status update and track

--- src/libraries/common.js
+++ src/libraries/common.js
@@ -14,6 +14,12 @@ module.exports.CONSENSUS_SOLO_VALUE = 0;
 module.exports.CONSENSUS_KAFKE = 'kafka';
 module.exports.CONSENSUS_KAFKA_VALUE = 1;
 
+// REQUEST STATUS SECTION
+module.exports.REQUEST_STATUS_PENDING = 'pending';
+module.exports.REQUEST_STATUS_RUNNING = 'running';
+module.exports.REQUEST_STATUS_SUCCESS = 'success';
+module.exports.REQUEST_STATUS_ERROR = 'error';
+
 module.exports.BOOTSTRAPUSER = {
     enrollmentID: 'admin',
     enrollmentSecret: 'adminpw'
