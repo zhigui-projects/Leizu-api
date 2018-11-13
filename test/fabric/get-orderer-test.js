b9f7849543ba34ad6de00104530bbd2ac7d98a9f
zhanghao <zhanghao@ziggurat.cn>
2018-11-13 15:55:52 +0800

    rebuild orderer interface

---
+++ test/fabric/get-orderer-test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const request = require('supertest');
+const app = require('../../src/index');
+const constants = require('./constants');
+let token = 'Bearer ' + constants.token;
+let id= '5be94315e7604f9f7653e600';
+request(app.callback())
+    .get('/api/v1/orderer/'+id)
+    .set('Authorization', token)
+    .expect(200)
+    .end(function (err, response) {
+        if (err) console.error(err);
+        console.log(response.body);
+        app.mongoose.disconnect();
+    });
