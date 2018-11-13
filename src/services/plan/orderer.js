7f8193b957e9397d53fa8e5368f1d7310af5c9d7
fanjiahe <fanjiahe@ziggurat.cn>
2018-11-13 09:53:12 +0800

    fill up method body

--- src/services/plan/orderer.js
+++ src/services/plan/orderer.js
@@ -6,6 +6,20 @@ module.exports = class OrdererPlan extends NodePlan {
 
     constructor(target){
         super(target);
+        this.planName = 'OrdererPlan';
+        this.targetName = 'OrdererTarget';
+    }
+
+    init(){
+        this.plan.target(this.targetName,target);
+        this.plan.remote(this.planName,function(remote){
+            // task lists;
+        });
+    }
+
+    run(){
+        this.plan.run(this.planName,this.targetName);
+        this.flushFinishedTasks();
     }
 
 };
