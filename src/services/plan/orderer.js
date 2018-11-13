e579f9566b682e361af5bc086fcf61addd5bf7b1
fanjiahe <fanjiahe@ziggurat.cn>
2018-11-13 09:33:29 +0800

    plan structure skeleton

---
+++ src/services/plan/orderer.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const NodePlan = require('./plan');
+
+module.exports = class OrdererPlan extends NodePlan {
+
+    constructor(target){
+        super(target);
+    }
+
+};
---
+++ src/services/plan/peer.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const NodePlan = require('./plan');
+
+module.exports = class PeerPlan extends NodePlan {
+
+    constructor(target){
+        super(target);
+    }
+
+};
