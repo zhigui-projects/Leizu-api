700ed5e316ed096b0244c5531934f247e83e1e7c
fanjiahe <fanjiahe@ziggurat.cn>
2018-11-13 09:53:12 +0800

    fill up method body

--- src/services/plan/peer.js
+++ src/services/plan/peer.js
@@ -6,6 +6,20 @@ module.exports = class PeerPlan extends NodePlan {
 
     constructor(target){
         super(target);
+        this.planName = 'PeerPlan';
+        this.targetName = 'PeerTarget';
+    }
+
+    init(){
+        this.plan.target(this.targetName,target);
+        this.plan.remote(this.planName,function(remote){
+            // task lists;
+        });
+    }
+
+    run(){
+        this.plan.run(this.planName,this.targetName);
+        this.flushFinishedTasks();
     }
 
 };
