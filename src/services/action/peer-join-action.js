765731f67bf9d0a2d72de3661b7e9803276585d3
fanjiahe <fanjiahe@ziggurat.cn>
2018-11-23 15:26:51 +0800

    refine codes

--- src/services/action/peer-join-action.js
+++ src/services/action/peer-join-action.js
@@ -4,7 +4,8 @@ const Action = require('./action');
 const utils = require('../../libraries/utils');
 const common = require('../../libraries/common');
 const DaoService = require('../db/dao');
-const JoinChannelService = require('../fabric/join-channel');
+const ChannelService = require('../fabric/channel');
+const FabricService = require('../db/fabric');
 
 module.exports = class PeerJoinAction extends Action {
 
@@ -15,38 +16,11 @@ module.exports = class PeerJoinAction extends Action {
     async execute(){
         let params = this.context.get(this.registry.CONTEXT.PARAMS);
         let channelName = params.channelName;
-        let peerId = params._id;
-        let networkConfig = await this.produceConfiguration(peerId);
-        networkConfig.orderConfig = params.params;
-        return await JoinChannelService.joinChannel(channelName,networkConfig);
-    }
-
-    async produceConfiguration(peerId){
-        let configuration = {};
-        let peer = await DaoService.findPeerById(peerId);
-        let peerObject = peer.toObject();
-        let organization = await DaoService.findOrganizationById(peerObject.org_id);
-        let orgObject = organization.toObject();
-        let certAuth = await DaoService.findCertAuthorityByOrg(orgObject._id);
-        let certAuthObject = certAuth.toObject();
-        configuration = {
-            caConfig: {
-                url: certAuthObject.url,
-                name: certAuthObject.name,
-                enrollId: common.BOOTSTRAPUSER.enrollmentID,
-                enrollSecret: common.BOOTSTRAPUSER.enrollmentSecret
-            },
-            newConfig: {
-                mspid: orgObject.msp_id,
-                url: peerObject.location,
-                pem: orgObject.root_cert,
-                adminKey: orgObject.admin_key,
-                adminCert: orgObject.admin_cert,
-                tlsKey: peerObject.tls_key,
-                tlsCert: peerObject.tls_cert
-            }
-        };
-        return configuration;
+        let organizationId = params.organization._id;
+        let channelService = await ChannelService.getInstance(organizationId, channelName);
+        let result = await channelService.joinChannel();
+        let channel = await FabricService.findChannelAndUpdate(channelId, result);
+        return channel;
     }
 
 };
