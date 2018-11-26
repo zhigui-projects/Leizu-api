f0ea97a6181656e116fde42e1e916ad5e079d446
Max Peng <pengisgood@gmail.com>
2018-11-26 12:21:54 +0800

    extract ids to one place for easier testing

--- test/fabric/constants.js
+++ test/fabric/constants.js
@@ -1,6 +1,9 @@
 'use strict';
 
 module.exports = {
-    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjViZDE2Njc1ZDk3MjU5OTdjMTBlMGQwNyIsImlhdCI6MTU0Mjg1NTYyNCwiZXhwIjoxNTQyODkxNjI0fQ.FYcM-PW_8Ag0DSm6Ee8BG7cEsBo9El1wjc8wq7b9-X8',
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjViZDE2Njc1ZDk3MjU5OTdjMTBlMGQwNyIsImlhdCI6MTU0MzE5Njc3NiwiZXhwIjoxNTQzMjMyNzc2fQ.ASQjt-vafCxV7Yey7m84CXQMvfmH2bXejLZWWnobKa8',
+    consortiumId: '5bfb7410eac1947a95b7e5d1',
+    organizationId: '5bfb7436f4793e7ab3118d0d',
+    channelId: '5bfb746dfa19647b08af94c7',
     help: 'These constants are used in the test cases'
 };
