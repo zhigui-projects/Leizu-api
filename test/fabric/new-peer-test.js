a89cf92768c00acc8f4c6714958c08825cf75289
Max Peng <pengisgood@gmail.com>
2018-11-26 12:21:54 +0800

    extract ids to one place for easier testing

--- test/fabric/new-peer-test.js
+++ test/fabric/new-peer-test.js
@@ -9,7 +9,7 @@ const peer = {
     username: 'root',
     password: 'Inkchain-passw0rd',
     port: 22,
-    organizationId: '5bf61facfd3da04705a04a1c'
+    organizationId: constants.organizationId
 };
 
 request(app.callback())
