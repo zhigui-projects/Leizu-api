e40e5021ba60642dd6886c3a5f519484fdb5e811
fanjiahe <fanjiahe@ziggurat.cn>
2018-11-27 11:10:57 +0800

    refine codes

--- src/services/action/kafka-provision-action.js
+++ src/services/action/kafka-provision-action.js
@@ -44,9 +44,11 @@ module.exports = class KafkaProvisionAction extends Action {
         let brokerList = [];
         for(let kafka of params.kafkas){
             sshClient.setOptions(kafka);
+            let hostname = kafka.name;
             let parameters = [
                 'create',
                 '--name', kafka.name,
+                '--hostname',hostname,
                 '--restart','always',
                 '-e', 'KAFKA_MESSAGE_MAX_BYTES=103809024',
                 '-e', 'KAFKA_REPLICA_FETCH_MAX_BYTES=103809024',
@@ -55,6 +57,11 @@ module.exports = class KafkaProvisionAction extends Action {
                 '-e', 'KAFKA_MIN_INSYNC_REPLICAS=2',
                 '-e', 'KAFKA_DEFAULT_REPLICATION_FACTOR=3',
                 '-e', 'KAFKA_ZOOKEEPER_CONNECT=' + kafkaZooKeeperConnect.join(','),
+                '-e', 'KAFKA_HOST_NAME='+hostname,
+                '-e', 'KAFKA_LISTENERS=EXTERNAL://0.0.0.0:9092,REPLICATION://0.0.0.0:9093',
+                '-e', 'KAFKA_ADVERTISED_LISTENERS=EXTERNAL://' + kafka.host + ':9092,REPLICATION://' + hostname + ':9093',
+                '-e', 'KAFKA_LISTENER_SECURITY_PROTOCOL_MAP=EXTERNAL:PLAINTEXT,REPLICATION:PLAINTEXT',
+                '-e', 'KAFKA_INTER_BROKER_LISTENER_NAME=REPLICATION',
                 '-p', '9092:9092',
                 'hyperledger/fabric-kafka'
             ];
