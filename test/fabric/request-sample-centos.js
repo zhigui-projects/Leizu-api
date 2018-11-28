1270d0d1e1b13ef6d48ba53b1cb59ff144d095e4
fanjiahe <fanjiahe@ziggurat.cn>
2018-11-28 16:12:21 +0800

    two orgs and two peers per org

--- test/fabric/request-sample-centos.js
+++ test/fabric/request-sample-centos.js
@@ -68,28 +68,50 @@ module.exports = {
             'ssh_password': 'FaVm&zu2mxFtkGwK'
         }]
     },
-    'peerOrgs': [{
-        'name': 'peer-org1',
-        'ca': {
-            'name': 'ca-2',
-            'ip': '39.104.170.33',
-            'ssh_username': 'root',
-            'ssh_password': 'FaVm&zu2mxFtkGwK'
+    'peerOrgs': [
+        {
+            'name': 'peer-org1',
+            'ca': {
+                'name': 'ca-2',
+                'ip': '39.104.170.33',
+                'ssh_username': 'root',
+                'ssh_password': 'FaVm&zu2mxFtkGwK'
+            },
+            'peers': [{
+                'name': 'peer0',
+                'ip': '39.104.132.206',
+                'ssh_username': 'root',
+                'ssh_password': 'FaVm&zu2mxFtkGwK'
+            }, {
+                'name': 'peer1',
+                'ip': '39.104.170.33',
+                'ssh_username': 'root',
+                'ssh_password': 'FaVm&zu2mxFtkGwK'
+            }]
         },
-        'peers': [{
-            'name': 'peer0',
-            'ip': '39.104.132.206',
-            'ssh_username': 'root',
-            'ssh_password': 'FaVm&zu2mxFtkGwK'
-        }, {
-            'name': 'peer1',
-            'ip': '39.104.170.33',
-            'ssh_username': 'root',
-            'ssh_password': 'FaVm&zu2mxFtkGwK'
-        }]
-    }],
+        {
+            'name': 'peer-org2',
+            'ca': {
+                'name': 'ca-3',
+                'ip': '39.104.134.22',
+                'ssh_username': 'root',
+                'ssh_password': 'FaVm&zu2mxFtkGwK'
+            },
+            'peers': [{
+                'name': 'peer0',
+                'ip': '39.104.134.22',
+                'ssh_username': 'root',
+                'ssh_password': 'FaVm&zu2mxFtkGwK'
+            }, {
+                'name': 'peer1',
+                'ip': '39.104.122.83',
+                'ssh_username': 'root',
+                'ssh_password': 'FaVm&zu2mxFtkGwK'
+            }]
+        }
+    ],
     'channel': {
         'name': 'mychannel',
-        'orgs': ['peer-org1']
+        'orgs': ['peer-org1','peer-org2']
     }
 };
