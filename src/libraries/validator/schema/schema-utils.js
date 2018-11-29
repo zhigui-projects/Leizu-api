489a17a1ee0b26e482ab09651f70449ab5fff535
zhanghao <zhanghao@ziggurat.cn>
2018-11-29 18:03:51 +0800

    remove required for port

--- src/libraries/validator/schema/schema-utils.js
+++ src/libraries/validator/schema/schema-utils.js
@@ -5,7 +5,7 @@ const Joi = require('joi');
 const objectId = Joi.string().length(24).required();
 const string = Joi.string().required();
 const hostname = Joi.string().hostname().required();
-const port = Joi.number().port().required();
+const port = Joi.number().port();
 const ip = Joi.string().ip().required();
 
 module.exports = {
