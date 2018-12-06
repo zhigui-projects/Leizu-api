6f3dc2b483dd7b630dcf9580ab0f634e7304c424
Max Peng <pengisgood@gmail.com>
2018-12-06 13:37:02 +0800

    import dotenv for environment variables
    https://github.com/motdotla/dotenv

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ Copyright Zhigui.com. All Rights Reserved.
 SPDX-License-Identifier: Apache-2.0
 */
 
+require('dotenv').config();
 const http = require('http');
 const app = require('./src');
 const port = 8080;
