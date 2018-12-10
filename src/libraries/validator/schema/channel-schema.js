cf30c007ea496604fe4de7a4200c19717fac5d84
zhanghao <zhanghao@ziggurat.cn>
2018-12-10 16:08:44 +0800

    update validate

--- src/libraries/validator/schema/channel-schema.js
+++ src/libraries/validator/schema/channel-schema.js
@@ -19,14 +19,14 @@ module.exports.getChannelDetail = Joi.object().options({abortEarly: true}).keys(
 });
 
 module.exports.createChannel = Joi.object().keys({
-    organizationIds: Joi.array().min(1).items(objectId).required(),
+    organizationIds: Joi.array().min(1).items(objectId).required().sparse(false),
     name: string,
 });
 
 module.exports.joinChannel = Joi.object().keys({
     organizationId: objectId,
     channelId: objectId,
-    peers: Joi.array().min(1).items(objectId),
+    peers: Joi.array().min(1).items(objectId).sparse(false),
 });
 
 module.exports.updateChannel = Joi.object().keys({
