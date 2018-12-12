e60271577c6c5aaca0f03b7fc9557b2038946a5c
Max Peng <pengisgood@gmail.com>
2018-12-12 17:14:42 +0800

    use unified toggle

--- src/services/action/action.js
+++ src/services/action/action.js
@@ -7,13 +7,14 @@ SPDX-License-Identifier: Apache-2.0
 'use strict';
 
 const Registry = require('./registry');
+const utils = require('../../libraries/utils');
 
 module.exports = class Action {
 
     constructor(){
         this.context = null;
         this.registry = Registry;
-        this.isDebugMode = false;
+        this.isDebugMode = utils.isSingleMachineTest();
     }
 
     setActionContext(context){
