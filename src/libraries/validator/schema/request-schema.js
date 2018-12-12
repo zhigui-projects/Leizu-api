9ff31eabec834be382b2d98fe5cb8e09de642abd
Max Peng <pengisgood@gmail.com>
2018-12-12 17:00:27 +0800

    update request validator

--- src/libraries/validator/schema/request-schema.js
+++ src/libraries/validator/schema/request-schema.js
@@ -10,16 +10,28 @@ const Joi = require('joi');
 const {string, ip} = require('./schema-utils');
 const Common = require('../../common');
 
-const serverSchema = {
-    name: string,
-    ip: ip,
-    ssh_username: string,
-    ssh_password: string
+const serverSchema = () => {
+    if (process.env.RUN_MODE === Common.RUN_MODE.REMOTE) {
+        return {
+            name: string,
+            ip: ip,
+            ssh_username: string,
+            ssh_password: string,
+        };
+    }
+    else {
+        return {
+            name: string,
+            ip: Joi.string().valid('127.0.0.1').required(),
+            ssh_username: Joi.any().forbidden(),
+            ssh_password: Joi.any().forbidden(),
+        };
+    }
 };
 
 const orgSchema = {
     name: string,
-    ca: Joi.object().keys(serverSchema).required()
+    ca: Joi.object().keys(serverSchema()).required()
 };
 
 const consensusCondition = {
@@ -33,13 +45,17 @@ module.exports.requestSchema = Joi.object().options({}).keys({
     version: Joi.string().valid(Common.VERSION_LIST).required(),
     db: Joi.string().valid(Common.DB_TYPE_LIST).required(),
     consensus: Joi.string().valid(Common.CONSENSUS_LIST).required(),
-    kafka: Joi.array().items(Joi.object().keys(serverSchema)).when('consensus', consensusCondition),
-    zookeeper: Joi.array().items(Joi.object().keys(serverSchema)).when('consensus', consensusCondition),
+    kafka: Joi.array().items(Joi.object().keys(serverSchema())).when('consensus', consensusCondition),
+    zookeeper: Joi.array().items(Joi.object().keys(serverSchema())).when('consensus', consensusCondition),
     ordererOrg: Joi.object().keys(Object.assign({
-        orderer: Joi.array().min(1).items(Joi.object().keys(serverSchema)).required().sparse(false)
+        name: string,
+        ca: Joi.object().keys(serverSchema()),
+        orderer: Joi.array().min(1).items(Joi.object().keys(serverSchema())).required().sparse(false)
     }, orgSchema)).required(),
     peerOrgs: Joi.array().min(1).items(Object.assign({
-        peers: Joi.array().min(1).items(Joi.object().keys(serverSchema)).required().sparse(false)
+        name: string,
+        ca: Joi.object().keys(serverSchema()),
+        peers: Joi.array().min(1).items(Joi.object().keys(serverSchema())).required().sparse(false)
     }, orgSchema)).required(),
     channel: Joi.object().keys({
         name: string,
