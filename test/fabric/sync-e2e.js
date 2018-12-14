48fc6fed12323940ca8bf3898756721bffb4ccc4
Max Peng <pengisgood@gmail.com>
2018-12-14 21:14:37 +0800

    use constants

--- test/fabric/sync-e2e.js
+++ test/fabric/sync-e2e.js
@@ -9,12 +9,10 @@ SPDX-License-Identifier: Apache-2.0
 const request = require('supertest');
 const app = require('../../src/index');
 const constants = require('./constants');
-
-const consortiumId = '5bc2a57c05ce040f0559a369';
 const token = 'Bearer ' + constants.token;
 
 request(app.callback())
-    .post('/api/v1/fabric/sync/' + consortiumId)
+    .post('/api/v1/fabric/sync/' + constants.consortiumId)
     .set('Authorization', token)
     .send({})
     .end(function(err,response){
